9429258b8421f6203acb18c7b01a6a673801c8b6
Maksmvs <142376726+Maksmvs@users.noreply.github.com>
2025-07-12 21:10:37 +0000

    start lesson-8-9 clean

--- lesson-7/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-s3-bucket-name"
-    key            = "lesson-7/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "your-lock-table"
-  }
-}
--- modules/s3-backend/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "bucket_name" {
-  type = string
-}
-
-variable "dynamodb_table_name" {
-  type = string
-}
